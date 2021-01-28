850aba4af53c598a125d2a60f3db421aaee8e87f
enrong.tsai <andy83523@gmail.com>
2021-01-28 19:54:11 +0800

    fix cv2 BGR

--- prepare_data.py
+++ prepare_data.py
@@ -42,6 +42,7 @@ def load_mx_rec(rec_path):
         img_info = imgrec.read_idx(idx)
         header, img = mx.recordio.unpack_img(img_info)
         label = int(header.label)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(img)
         label_path = save_path/str(label)
         if not label_path.exists():
@@ -53,7 +54,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='for face verification')
     parser.add_argument("-r", "--rec_path", help="mxnet record file path", default='faces_emore', type=str)
     args = parser.parse_args()
-    rec_path = Path('dataset')/args.rec_path
+    rec_path = Path('data')/args.rec_path
     load_mx_rec(rec_path)
 
     bin_files = ['agedb_30', 'cfp_fp', 'lfw', 'calfw', 'cfp_ff', 'cplfw', 'vgg2_fp']
